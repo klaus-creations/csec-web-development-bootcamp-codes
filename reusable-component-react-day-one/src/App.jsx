0b5adb9d470302278989f21e319a3741ee07a724
Abush Natanem <abushnatanem@gmail.com>
2025-01-30 22:50:22 +0300

    initialize the app wit vite and tailwincss

---
+++ reusable-component-react-day-one/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-full h-[100vh] bg-gray-800">
+      <h1 className="text-bold text-white font-bold tracking-[1px]">
+        Hello csec Web Development Bootcamp
+      </h1>
+    </div>
+  );
+}
+
+export default App;
