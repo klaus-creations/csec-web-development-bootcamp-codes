ff99840d6e6ad882ba291d03db5ee36ce5ebe9a6
Abush Natanem <abushnatanem@gmail.com>
2025-03-02 16:04:47 +0300

    update job Route

--- express-trial/routes/job.route.js
+++ express-trial/routes/job.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { getJobs, getSingleJob } from "../controllers/job.controller.js";
+import {
+  deleteJob,
+  getJobs,
+  getSingleJob,
+} from "../controllers/job.controller.js";
 import { postJob } from "../controllers/job.controller.js";
 
 const jobRouter = Router();
@@ -7,5 +11,6 @@ const jobRouter = Router();
 jobRouter.get("/", getJobs);
 jobRouter.get("/:jobId", getSingleJob);
 jobRouter.post("/new", postJob);
+jobRouter.delete("/delete/:id", deleteJob);
 
 export default jobRouter;
