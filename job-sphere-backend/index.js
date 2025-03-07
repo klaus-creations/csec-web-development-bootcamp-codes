dc5b5cb208f1a815ea241f1f328bf8eefc85c7a4
Abush Natanem <abushnatanem@gmail.com>
2025-03-07 09:28:24 +0300

    add the cors

--- job-sphere-backend/index.js
+++ job-sphere-backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import { PORT } from "./config/env.js";
 import connectDB from "./database/db.js";
@@ -7,6 +8,7 @@ import jobRouter from "./routes/job.route.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/jobs/", jobRouter);
 
