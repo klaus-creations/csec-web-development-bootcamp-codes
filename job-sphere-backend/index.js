e5f0d1cbf5a1c004e0af4e8c95a2d2f4573f23f2
Abush Natanem <abushnatanem@gmail.com>
2025-03-14 13:17:30 +0300

    update th server file

--- job-sphere-backend/index.js
+++ job-sphere-backend/index.js
@@ -5,12 +5,14 @@ import { PORT } from "./config/env.js";
 import connectDB from "./database/db.js";
 
 import jobRouter from "./routes/job.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/jobs/", jobRouter);
+app.use("/auth", authRouter);
 
 app.listen(PORT, async function () {
   console.log(`Server running on port ${PORT} port`);
