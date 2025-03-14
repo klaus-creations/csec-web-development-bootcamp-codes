bd3ebfc7cb0ed2e9a360fdf00f36e3e82118c3a3
Abush Natanem <abushnatanem@gmail.com>
2025-03-14 13:26:57 +0300

    update index.js

--- job-sphere-backend/index.js
+++ job-sphere-backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/jobs/", jobRouter);
-app.use("/api/auth", authRouter);
+app.use("/api/auth/", authRouter);
 
 app.listen(PORT, async function () {
   console.log(`Server running on port ${PORT} port`);
