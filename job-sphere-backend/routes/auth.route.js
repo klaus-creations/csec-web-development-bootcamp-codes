a359aae8dfd41224f4d387b860d3bf9fb7a00221
Abush Natanem <abushnatanem@gmail.com>
2025-03-14 13:24:18 +0300

    update the auth router with validation

--- job-sphere-backend/routes/auth.route.js
+++ job-sphere-backend/routes/auth.route.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { signIn, signOut, signUp } from "../controllers/auth.controller";
+import {
+  handleValidationErrors,
+  signInValidations,
+  signUpValidations,
+} from "../middlewares/validator";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", signUp);
-authRouter.post("/sign-in", signIn);
+authRouter.post("/sign-up", signUpValidations, handleValidationErrors, signUp);
+authRouter.post("/sign-in", signInValidations, handleValidationErrors, signIn);
 authRouter.post("/sign-out", signOut);
 
 export default authRouter;
