d8b0f4c16755b9c26ef3c2c8664756fce74bdb03
Abush Natanem <abushnatanem@gmail.com>
2025-03-14 13:42:32 +0300

    add jwt expires in

--- job-sphere-backend/config/env.js
+++ job-sphere-backend/config/env.js
@@ -1,4 +1,4 @@
 import { config } from "dotenv";
 
 config({ path: `.env` });
-export const { PORT, DB_URI, JWT_SECRET } = process.env;
+export const { PORT, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
